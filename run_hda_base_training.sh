a19491ec0f7ef7f13ec496a1fc7122f9e8f06647
Yihang She <yihshe@biwidl13.ee.ethz.ch>
2021-08-10 10:13:28 +0200

    small change

--- run_hda_base_training.sh
+++ run_hda_base_training.sh
@@ -6,9 +6,9 @@
 source /scratch_net/biwidl13/yihshe/conda/etc/profile.d/conda.sh
 conda activate pytcu10
 
-# python3 -m tools.train_net --num-gpus 2 \
-#         --config-file configs/COCO-detection/faster_rcnn_R_101_FPN_hda_base.yaml 
+python3 -m tools.train_net --num-gpus 2 \
+        --config-file configs/COCO-detection/faster_rcnn_R_101_FPN_hda_base.yaml 
 
-python3 -m tools.test_net --num-gpus 2\
-        --config-file configs/COCO-detection/faster_rcnn_R_101_FPN_hda_base.yaml \
-        --eval-only
+# python3 -m tools.test_net --num-gpus 2\
+#         --config-file configs/COCO-detection/faster_rcnn_R_101_FPN_hda_base.yaml \
+#         --eval-only
